92ef513acb1177762b0a2d9248c708d396cd6a75
YCL069 <145255995+YCL069@users.noreply.github.com>
2023-10-10 20:12:47 +0800

    Add files via upload

--- js/3d.js
+++ js/3d.js
@@ -5,20 +5,26 @@ import Stats from 'three/addons/libs/stats.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
 import { MMDLoader } from 'three/addons/loaders/MMDLoader.js';
-import { getUrlParams } from './function.module.js';
 
 let stats;
 let camera, scene, renderer, effect, composer;
-// 获取网页参数
+let info = document.getElementById('info');
+// 获取网页URL参数
 const name = getUrlParams("name");
-const number = getUrlParams("weapons")
+const number = getUrlParams("weapons");
 const file = `./models/${name}/index.pmx`;
 // 主函数
-Ammo().then(function (AmmoLib) {
-  Ammo = AmmoLib;
-  init(file);
-  animate();
-});
+try {
+  Ammo().then(function (AmmoLib) {
+    Ammo = AmmoLib;
+    init(file);
+    animate();
+  });
+} catch (err) {
+  info.style.width = "100%";
+  info.style.backgroundColor = "red";
+  info.innerHTML = "模型加载器初始化出错, 请刷新页面以重新加载!<br>如多次出现初始化错误,请将下面的详细内容复制并<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a><br><hr>" + err;
+}
 // 场景配置
 function init(file) {
   const container = document.createElement('div');
@@ -48,29 +54,41 @@ function init(file) {
   container.appendChild(renderer.domElement);
   container.appendChild(stats.dom);
   // 提示信息
-  let htmlObj = document.getElementById('text_div');
-  var a = document.createElement('h3');
-  a.innerHTML = "模型加载器初始化成功.";
-  htmlObj.appendChild(a);
-  var a = document.createElement('h3');
+  info.innerHTML = "模型加载器初始化成功.<hr>若页面长时间未响应请刷新页面!<br>如多次出现,请<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a>";
   // 人物模型
   const loader = new MMDLoader();
-  loader.load(file, function (mesh) {
-    // 添加到屏幕( X:0 y:-10 Z:0)
-    mesh.position.y = -10;
-    scene.add(mesh);
-    // 提示信息
-    var a = document.createElement('h3');
-    a.innerHTML = "模型主文件加载完成<br><br>开始加载模型材质...<br><br><a style='color:red'>无法获取进度,请自行判断模型材质加载进度!</a><br><a style='color:aqua'></a>";
-    htmlObj.appendChild(a);
-    setTimeout(function () {
-      document.getElementById('progrsess').style.display = "none";
-    }, 4000);
-  },
-    function (xhr) {
+  loader.load(
+    file,
+    (mesh) => {
+      // 添加到屏幕( X:0 y:-10 Z:0)
+      mesh.position.y = -10;
+      scene.add(mesh);
       // 提示信息
-      a.innerHTML = "加载模型主文件..." + (xhr.loaded / xhr.total * 100).toFixed(4) + "%";
-      htmlObj.appendChild(a);
+      if (document.getElementById('error').innerHTML == 1) {
+        info.style.width = "50%";
+        info.style.backgroundColor = "goldenrod";
+        info.innerHTML = "模型加载器出现非致命性错误, 若无法正常渲染请刷新页面!<br>如多次出现渲染错误,请<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a>"
+        setTimeout(() => {
+          document.getElementById('progrsess').style.transition = "3s"
+          document.getElementById('progrsess').style.top = "-200px";
+        }, 1000);
+      } else {
+        info.innerHTML = "预处理模型材质..."
+        onAfterRender(mesh, 0, 0, () => {
+          info.style.backgroundColor = "green";
+          info.innerHTML = "模型材质预处理成功,等待GPU渲染.";
+          setTimeout(() => {
+            document.getElementById('progrsess').style.top = "-35px";
+          }, 1000);
+        });
+      }
+    },
+    (xhr) => {
+      info.innerHTML = "加载模型主文件..." + (xhr.loaded / xhr.total * 100).toFixed(2) + "%<br>" + (xhr.loaded / 1024).toFixed(0) + " KB / " + (xhr.total / 1024).toFixed(0) + " KB";
+    },
+    (err) => {
+      document.getElementById('error').innerHTML = "1";
+      console.error(err);
     }
   );
   // 武器模型 
@@ -119,10 +137,10 @@ function weapons(loader, number) {
         scene.add(mesh);
       });
     }
-  } else if (number = 4 && number != 7) { 
+  } else if (number = 4 && number != 7) {
     // 姬子
     let x = [0, -20, +20, +10, -10, -20];
-    let z = [0, 0,   0,   -20, -20, -20];
+    let z = [0, 0, 0, -20, -20, -20];
     for (let i = 1; i <= number; i++) {
       loader.load(`./models/${name}/${i}.pmx`, function (mesh) {
         // 添加到屏幕(X,Y,Z)
@@ -134,14 +152,9 @@ function weapons(loader, number) {
     }
   } else if (number = 7) {
     // 玲可
-    let x = [0, -15, +20, +10, -10, -20, 0,   +20];
-    let z = [0, 0,   0,   -15, -15, -15, -15, -15];
+    let x = [0, -15, +20, +10, -10, -20, 0, +20];
+    let z = [0, 0, 0, -15, -15, -15, -15, -15];
     for (let i = 1; i <= number; i++) {
-
-      console.log(i);
-      console.log(x[i]);
-      console.log(z[i]);
-
       loader.load(`./models/${name}/${i}.pmx`, function (mesh) {
         // 添加到屏幕(X,Y,Z)
         mesh.position.x = x[i];
@@ -151,4 +164,61 @@ function weapons(loader, number) {
       });
     }
   }
+}
+/**
+ * 获取模型当前已渲染的贴图数
+ * @param model       模型对象
+ * @returns {number}  贴图数
+ */
+function getImageCount(model) {
+  let iCount = 0
+
+  model.traverse(item => {
+    if (item instanceof THREE.Mesh) {
+      if (Array.isArray(item.material)) {
+        item.material.forEach(m => {
+          if (m.map && m.map.image) {
+            iCount++
+          }
+        })
+      } else {
+        if (item.material.map && item.material.map.image) {
+          iCount++
+        }
+      }
+    }
+  })
+
+  return iCount
+}
+/**
+ *
+ * @param model     模型对象
+ * @param ic        贴图计数器
+ * @param ec        重复次数
+ * @param callback  回调函数
+ */
+function onAfterRender(model, ic, ec, callback) {
+  const wait = 3
+
+  let iCount = ic
+
+  let equalCount = ec
+
+  setTimeout(() => {
+    const res = getImageCount(model)
+
+    if (iCount === res) {
+      if (equalCount === wait) {
+        callback()
+      } else {
+        equalCount++
+        onAfterRender(model, iCount, equalCount, callback)
+      }
+    } else {
+      iCount = res
+      equalCount = 0
+      onAfterRender(model, iCount, equalCount, callback)
+    }
+  }, 100)
 }
