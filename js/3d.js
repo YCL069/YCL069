fcc4d41fc7b93a91d7c5bb7f206a743ce94dd995
YCL069 <145255995+YCL069@users.noreply.github.com>
2023-12-25 18:25:10 +0800

    Add files via upload

--- js/3d.js
+++ js/3d.js
@@ -5,19 +5,38 @@ import Stats from 'three/libs/stats.module.js';
 import { OrbitControls } from 'three/controls/OrbitControls.js';
 import { OutlineEffect } from 'three/effects/OutlineEffect.js';
 import { MMDLoader } from 'three/loaders/MMDLoader.js';
+import { MMDAnimationHelper } from 'three/animation/MMDAnimationHelper.js';
 
 let stats;
+let helper, mesh;
 let camera, scene, renderer, effect, composer;
 let info = document.getElementById('info');
-// 获取网页URL参数
-const name = getUrlParams("name");
-const number = getUrlParams("weapons");
-const file = `https://ycl069.github.io/models/${name}/index.pmx`;
+const clock = new THREE.Clock();
+
+// 处理传入参数
+var id = getUrlParams('id');
+var vmd = getUrlParams('vmd');
+var other = getUrlParams('other');
+if (typeof id === 'undefined') {
+  id = 1
+}
+if (!other) {
+  var dataurl = "./data.json"
+} else {
+  var dataurl = "./data2.json"
+}
+
+// 判断是否为Webkit用户
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+if (browser().engine == "webkit" || isSafari) {
+  alert("!!!注意!!!\n由于Webkit内核对于WebGL兼容性有限,  页面可能出现未知渲染问题。\n若可能请更换非Webkit内核浏览器访问")  
+}
+
 // 主函数
 try {
   Ammo().then(function (AmmoLib) {
     Ammo = AmmoLib;
-    init(file);
+    init();
     animate();
   });
 } catch (err) {
@@ -25,8 +44,9 @@ try {
   info.style.backgroundColor = "red";
   info.innerHTML = "模型加载器初始化出错, 请刷新页面以重新加载!<br>如多次出现初始化错误,请将下面的详细内容复制并<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a><br><hr>" + err;
 }
+
 // 场景配置
-function init(file) {
+function init() {
   const container = document.createElement('div');
   document.body.appendChild(container);
   // 相机
@@ -45,6 +65,7 @@ function init(file) {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
   // 渲染器
   composer = new EffectComposer(renderer);
   composer.addPass(new RenderPass(scene, camera));
@@ -54,53 +75,127 @@ function init(file) {
   container.appendChild(renderer.domElement);
   container.appendChild(stats.dom);
   // 提示信息
-  info.innerHTML = "模型加载器初始化成功.<hr>若页面长时间未响应请刷新页面!<br>如多次出现,请<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a>";
+  info.innerHTML = "模型加载器初始化成功. <hr>若页面长时间未响应请刷新页面!<br>如多次出现,请<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a>";
   // 人物模型
   const loader = new MMDLoader();
-  loader.load(
-    file,
-    (mesh) => {
-      // 添加到屏幕( X:0 y:-10 Z:0)
-      mesh.position.y = -10;
-      scene.add(mesh);
-      // 提示信息
-      if (document.getElementById('error').innerHTML == 1) {
-        info.style.width = "50%";
-        info.style.backgroundColor = "goldenrod";
-        info.innerHTML = "模型加载器出现非致命性错误, 若无法正常渲染请刷新页面!<br>如多次出现渲染错误,请<a href='https://ycl.cool/blog/index.php/archives/17/'>点此反馈.</a>"
-        setTimeout(() => {
-          info.innerHTML = "";
-          document.getElementById('progrsess').style.transition = "3s"
-          document.getElementById('progrsess').style.top = "-200px";
-        }, 2000);
-      } else {
-        info.innerHTML = "预处理模型材质..."
-        onAfterRender(mesh, 0, 0, () => {
-          info.style.backgroundColor = "green";
-          info.innerHTML = "模型材质预处理成功,等待GPU渲染.";
-          setTimeout(() => {
-            info.innerHTML = "";
-            document.getElementById('progrsess').style.top = "-35px";
-          }, 1000);
-        });
-      }
-    },
-    (xhr) => {
-      info.innerHTML = "加载模型主文件..." + (xhr.loaded / xhr.total * 100).toFixed(2) + "%<br>" + (xhr.loaded / 1024).toFixed(0) + " KB / " + (xhr.total / 1024).toFixed(0) + " KB";
-    },
-    (err) => {
-      document.getElementById('error').innerHTML = "1";
-      console.error(err);
+  helper = new MMDAnimationHelper();
+  json(dataurl, id, "name", (name) => {
+    const pmxfile = `./models/${name}/index.pmx`;
+    // const pmxfile = `https://ycl069.github.io/models/${name}/index.pmx`;
+    if (!vmd) {
+      loader.load(
+        pmxfile,
+        (mesh) => {
+          // 添加到屏幕( X:0 y:-10 Z:0)
+          mesh.position.y = -10;
+          scene.add(mesh);
+          // 提示信息
+          Finish();
+        },
+        (xhr) => {
+          info.innerHTML = "加载模型主文件..." + (xhr.loaded / xhr.total * 100).toFixed(2) + "%<br>" + (xhr.loaded / 1024).toFixed(0) + " KB / " + (xhr.total / 1024).toFixed(0) + " KB";
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+      // 武器模型
+      json(dataurl, id, "weapons", (number) => {
+        console.log("ID:" + id + " Name:" + name + " Weapons:" + number);
+        if (number) {
+          weapons(loader, name, number);
+        }
+      })
+    } else {
+      loader.loadWithAnimation(
+        pmxfile,
+        `./vmd/${vmd}/index.vmd`,
+        (mmd) => {
+          // 添加到屏幕( X:0 y:-10 Z:0)
+          mesh = mmd.mesh;
+          mesh.position.y = -10;
+          scene.add(mesh);
+          // 监听
+          const audioListener = new THREE.AudioListener();
+          camera.add(audioListener);
+          // 音频对象
+          const oceanAmbientSound = new THREE.Audio(audioListener);
+          scene.add(oceanAmbientSound);
+          // 加载音频资源
+          const loader2 = new THREE.AudioLoader();
+          info.style.width = "250px";
+          loader2.load(
+            `./vmd/${vmd}/index.mp3`,
+            (audioBuffer) => {
+              // 按钮触发
+              var Btn = document.createElement('button');
+              Btn.innerText = "开始";
+              Btn.onclick = () => {
+                oceanAmbientSound.setBuffer(audioBuffer);
+                helper.add(mesh, {
+                  animation: mmd.animation,
+                  physics: true
+                });
+                // 播放音频
+                oceanAmbientSound.play();
+                // 提示信息
+                info.innerHTML = "请稍等..."
+                setTimeout(() => {
+                  document.getElementById('progrsess').style.transition = "3s"
+                  document.getElementById('progrsess').style.top = "-200px";
+                  // 防止屏幕上方出现一条黑边
+                  setTimeout(() => {
+                    document.getElementById('progrsess').style.display = "none";
+                  }, 3000)
+                }, 1500)
+              }
+              // 提示信息2
+              document.getElementById('VMDList').style.left = "0px";
+              info.style.backgroundColor = "#00ff00a4";
+              json("./vmd/data.json", vmd, "name", (mmdinfo3) => {
+                json("./vmd/data.json", vmd, "from", (mmdinfo2) => {
+                  if (!other) {
+                    console.log("ID:" + id + " Name:" + name + " ModelFrom:神帝宇 AnimationID:" + vmd + " AnimationName:" + mmdinfo3 + " AnimationFrom:" + mmdinfo2);
+                    info.innerHTML = `请等待材质加载完成后点击按钮.<hr>模型来源: 神帝宇 <br>动作来源: ${mmdinfo2} <br>背景音乐: ${mmdinfo3}<br>制作软件: three.js<hr>`;
+                    info.appendChild(Btn);
+                  } else {
+                    json(dataurl, id, "from", (mmdinfo1) => {
+                      console.log("ID:" + id + " Name:" + name + " ModelFrom:" + mmdinfo1 + " AnimationID:" + vmd + " AnimationName:" + mmdinfo3 + " AnimationFrom:" + mmdinfo2);
+                      info.innerHTML = `请等待材质加载完成后点击按钮.<hr>模型来源: ${mmdinfo1}<br>动作来源: ${mmdinfo2}<br>背景音乐: ${mmdinfo3}<br>制作软件: three.js<hr>`;
+                      info.appendChild(Btn);
+                    })
+                  }
+                })
+              })
+              document.getElementById('three').style.top = "-20px"
+            },
+            // 声音回调函数
+            (xhr) => {
+              info.innerHTML = "加载音频文件..." + (xhr.loaded / xhr.total * 100).toFixed(2) + "%<br>" + (xhr.loaded / 1024).toFixed(0) + " KB / " + (xhr.total / 1024).toFixed(0) + " KB";
+            },
+            (err) => {
+              console.error(err);
+            }
+          )
+        },
+        // 模型和动作回调函数
+        (xhr) => {
+          info.innerHTML = "加载模型文件和动作文件..." + (xhr.loaded / xhr.total * 100).toFixed(2) + "%<br>" + (xhr.loaded / 1024).toFixed(0) + " KB / " + (xhr.total / 1024).toFixed(0) + " KB";
+        },
+        (err) => {
+          console.error(err);
+        }
+      )
     }
-  );
-  // 武器模型 
-  weapons(loader, number);
-  // 相机事件
+  })
+
+  // 相机
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.minDistance = 10;
   controls.maxDistance = 100;
   window.addEventListener('resize', onWindowResize);
 }
+
 // 相机事件
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -109,6 +204,7 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   composer.setSize(window.innerWidth, window.innerHeight);
 }
+
 // 渲染场景
 function animate() {
   requestAnimationFrame(animate);
@@ -117,11 +213,20 @@ function animate() {
   renderer.toneMappingExposure = Math.pow(1.0, 1);
   renderer.render(scene, camera);
   stats.end();
+  render();
 }
+
+// 动画
+function render() {
+  helper.update(clock.getDelta());
+  renderer.render(scene, camera);
+}
+
 // 加载武器模型
-function weapons(loader, number) {
+function weapons(loader, name, number) {
+  console.log(number)
   if (number == 1) {
-    loader.load(`https://ycl069.github.io/models/${name}/1.pmx`, function (mesh) {
+    loader.load(`./models/${name}/1.pmx`, function (mesh) {
       // 添加到屏幕( X:-10 y:-10 Z:0)
       mesh.position.x = -10;
       mesh.position.y = -10;
@@ -131,7 +236,8 @@ function weapons(loader, number) {
     let x = [0, -10, +10, +5, -5, -10];
     let z = [0, 0, 0, -10, -10, 0];
     for (let i = 1; i <= number; i++) {
-      loader.load(`https://ycl069.github.io/models/${name}/${i}.pmx`, function (mesh) {
+      console.log("sd" + i)
+      loader.load(`./models/${name}/${i}.pmx`, (mesh) => {
         // 添加到屏幕(X,Y,Z)
         mesh.position.x = x[i];
         mesh.position.y = -10;
@@ -139,25 +245,13 @@ function weapons(loader, number) {
         scene.add(mesh);
       });
     }
-  } else if (number = 4 && number != 7) {
-    // 姬子
-    let x = [0, -20, +20, +10, -10, -20];
-    let z = [0, 0, 0, -20, -20, -20];
-    for (let i = 1; i <= number; i++) {
-      loader.load(`https://ycl069.github.io/models/${name}/${i}.pmx`, function (mesh) {
-        // 添加到屏幕(X,Y,Z)
-        mesh.position.x = x[i];
-        mesh.position.y = -10;
-        mesh.position.z = z[i];
-        scene.add(mesh);
-      });
-    }
-  } else if (number = 7) {
-    // 玲可
+  } else if (number > 4) {
+    // 姬子、玲可
     let x = [0, -15, +20, +10, -10, -20, 0, +20];
     let z = [0, 0, 0, -15, -15, -15, -15, -15];
     for (let i = 1; i <= number; i++) {
-      loader.load(`https://ycl069.github.io/models/${name}/${i}.pmx`, function (mesh) {
+      console.log("sd" + i)
+      loader.load(`./models/${name}/${i}.pmx`, (mesh) => {
         // 添加到屏幕(X,Y,Z)
         mesh.position.x = x[i];
         mesh.position.y = -10;
@@ -167,60 +261,41 @@ function weapons(loader, number) {
     }
   }
 }
-/**
- * 获取模型当前已渲染的贴图数
- * @param model       模型对象
- * @returns {number}  贴图数
- */
-function getImageCount(model) {
-  let iCount = 0
 
-  model.traverse(item => {
-    if (item instanceof THREE.Mesh) {
-      if (Array.isArray(item.material)) {
-        item.material.forEach(m => {
-          if (m.map && m.map.image) {
-            iCount++
-          }
-        })
-      } else {
-        if (item.material.map && item.material.map.image) {
-          iCount++
+// 加载完成提示信息
+function Finish() {
+  // 关闭按钮
+  var ok = document.createElement('button');
+  ok.innerText = "关闭";
+  ok.onclick = () => {
+    document.getElementById('progrsess').style.transition = "3s"
+    document.getElementById('progrsess').style.top = "-200px";
+    // 防止屏幕上方出现一条黑边
+    setTimeout(() => {
+      document.getElementById('progrsess').style.display = "none";
+    }, 3000)
+  }
+  info.style.backgroundColor = "#00ff00a4";
+  info.style.width = "250px";
+  if (!other) {
+    info.innerHTML = "加载完成! 请等待模型贴图下载.<hr>模型来源: 神帝宇<hr>";
+    info.appendChild(ok);
+    setTimeout(() => {
+      document.getElementById('VMDList').style.left = "0px";
+      document.getElementById('three').style.top = "-20px"
+    }, 1500)
+  } else {
+    json(dataurl, id, "from", (from) => {
+      info.innerHTML = `加载完成! 请等待模型贴图下载.<hr>模型来源: ${from}<hr>`;
+      info.appendChild(ok);
+    })
+    setTimeout(() => {
+      json(dataurl, id, "mmd", (CanVMD) => {
+        if (CanVMD) {
+          document.getElementById('VMDList').style.left = "0px";
         }
-      }
-    }
-  })
-
-  return iCount
-}
-/**
- *
- * @param model     模型对象
- * @param ic        贴图计数器
- * @param ec        重复次数
- * @param callback  回调函数
- */
-function onAfterRender(model, ic, ec, callback) {
-  const wait = 3
-
-  let iCount = ic
-
-  let equalCount = ec
-
-  setTimeout(() => {
-    const res = getImageCount(model)
-
-    if (iCount === res) {
-      if (equalCount === wait) {
-        callback()
-      } else {
-        equalCount++
-        onAfterRender(model, iCount, equalCount, callback)
-      }
-    } else {
-      iCount = res
-      equalCount = 0
-      onAfterRender(model, iCount, equalCount, callback)
-    }
-  }, 100)
+      })
+      document.getElementById('three').style.top = "-20px"
+    }, 1500)
+  }
 }
